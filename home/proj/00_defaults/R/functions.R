4bcc57615efe3b971c7b437507fc5a8464a1b170
apereira <ailton.pereira@leibniz-hki.de>
2026-03-05 13:45:34 +0100

    Fix alignment

--- home/proj/00_defaults/R/functions.R
+++ home/proj/00_defaults/R/functions.R
@@ -8,18 +8,18 @@ apereira_theme <- function(ratio = 1.1, font = "Arial", ...) {
   
   theme_bw() %+replace%
     theme(
-      plot.tag     = element_text(size = rel(ratio), family = font),
-      strip.text   = element_text(size = rel(ratio), family = font),
-      axis.text.x    = element_text(size = rel(ratio), family = font),
-      axis.text.y    = element_text(size = rel(ratio), family = font),
-      axis.title   = element_text(size = rel(ratio), family = font),
-      legend.text  = element_text(size = rel(ratio), family = font),
-      legend.title = element_text(size = rel(ratio), family = font),
-      title              = element_text(size = rel(ratio), family = font),
-      panel.grid.minor.x = element_blank(),
-      panel.grid.major.x = element_blank(),
-      strip.background   = element_blank(),
-      panel.border       = element_rect(colour = "black", fill = NA),
+      plot.tag            = element_text(size = rel(ratio), family = font),
+      strip.text          = element_text(size = rel(ratio), family = font),
+      axis.text.x         = element_text(size = rel(ratio), family = font),
+      axis.text.y         = element_text(size = rel(ratio), family = font),
+      axis.title          = element_text(size = rel(ratio), family = font),
+      legend.text         = element_text(size = rel(ratio), family = font),
+      legend.title        = element_text(size = rel(ratio), family = font),
+      title               = element_text(size = rel(ratio), family = font),
+      panel.grid.minor.x  = element_blank(),
+      panel.grid.major.x  = element_blank(),
+      strip.background    = element_blank(),
+      panel.border        = element_rect(colour = "black", fill = NA),
       ...
     )
 }
